a59d0d8b66733af1ab0206726365e667dcccc3d5
HelenYazh <olenka.yazh05@gmail.com>
2024-10-05 01:42:50 +0300

    add scripts

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,17 @@
-export const removeLastContact = async () => {};
+import { readContacts } from "../utils/readContacts.js";
+import { writeContacts } from "../utils/writeContacts.js";
+
+export const removeLastContact = async () => {
+    try {
+        const data = await readContacts();
+        if (data.length > 1) {
+            data.pop();
+            return writeContacts(data);
+        }
+
+    } catch (error) {
+        console.error(error);
+    }
+};
 
 removeLastContact();
